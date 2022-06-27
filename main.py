7116eefba0a395cd8ab1202bde09ef204c118d6c
Claudia Ibarra <Smaragdus7@users.noreply.github.com>
2022-06-27 14:00:25 -0500

    Error Handling added

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import pandas
 
 nato_dictionary = pandas.read_csv("nato_phonetic_alphabet.csv")
-
 new_dictionary = {row.letter: row.code for (index, row) in nato_dictionary.iterrows()}
 
-word = input("Type a word: ").upper()
 
-word_code = [new_dictionary[letter] for letter in word]
-print(word_code)
+def generate_phonetic():
+    word = input("Type a word: ").upper()
+    try:
+        word_code = [new_dictionary[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters allowed.")
+        generate_phonetic()
+    else:
+        print(word_code)
 
+
+generate_phonetic()
